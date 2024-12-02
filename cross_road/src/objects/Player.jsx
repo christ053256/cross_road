f8c8b84346b9e65124ee823b3a1a9897824edc48
Kirisukyoto <christian.isiderio@carsu.edu.ph>
2024-12-02 08:51:06 +0000

    Setup Day 2

---
+++ cross_road/src/objects/Player.jsx
@@ -0,0 +1,3 @@
+// Instructions: Create a player (Static is fine) for your project. 
+// Find a low-resolution asset (1K or lower) on the internet. 
+// Only assets with a resolution of 1K or below are allowed.
